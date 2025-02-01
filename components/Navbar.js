dd99766d4f7a7cf1d27a34930ea0cb0bcce6aae8
Shivam <shivamnauriyal1224@gmail.com>
2025-02-01 20:52:40 +0530

    first manual commit for future culture

---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between p-2">
+      <span className="text-lg font-bold">LOGO</span>
+      <div className="p-2">☰</div>
+    </div>
+  );
+};
+
+export default Navbar;

1ce49a177214e75248217771e6ca4ab2513de94a
Shivam <shivamnauriyal1224@gmail.com>
2025-02-05 23:19:05 +0530

    Added stats

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Collections from "./Collections";
 import FeaturedSets from "./FeaturedSets";
 import HowItWorks from "./HowItWorks";
 import Testimonials from "./Testimonials";
+import Stats from "./Stats";
 
 const page = () => {
   return (
@@ -15,6 +16,7 @@ const page = () => {
       <FeaturedSets />
       <HowItWorks />
       <Testimonials />
+      <Stats />
       <h1 className="mb-24">thiw is home </h1>
       <h1 className="mb-24">thiw is home </h1>
       <h1 className="mb-24">thiw is home </h1>
